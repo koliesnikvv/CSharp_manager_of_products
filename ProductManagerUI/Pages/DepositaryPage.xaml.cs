bc52fedac44a77f5f96c7cdbdf3f167982c40097
Koliesnikova Vieronika <koliesnnikova@gmail.com>
2026-02-25 22:19:57 +0200

    modyfied main window and main frame, also created a folder for future pages like depositaries, products, depositary and products details

---
+++ ProductManagerUI/Pages/DepositaryPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductManagerUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for DepositaryPage.xaml
+    /// </summary>
+    public partial class DepositaryPage : Page
+    {
+        public DepositaryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
