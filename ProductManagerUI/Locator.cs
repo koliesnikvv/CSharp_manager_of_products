438e68a61b9a40686f6b50dc94ccdcfcd82b660b
Koliesnikova Vieronika <koliesnnikova@gmail.com>
2026-02-25 22:00:16 +0200

    added a file for future dependency injection

---
+++ ProductManagerUI/Locator.cs
@@ -0,0 +1,16 @@
+﻿using ServicesClasses;
+namespace ProductManagerUI
+{
+    public static class Locator
+    {
+        public static IDataService DataService
+        { 
+            get; private set; 
+        }
+        public static void Init()
+        {
+            IStorageInit storage = new StarterStorage();
+            DataService = new ProcessingStorage(storage);
+        }
+    }
+}
