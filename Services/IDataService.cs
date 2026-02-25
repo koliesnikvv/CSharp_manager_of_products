c2691280c4915734ae57ed0baeca95c33d9b6fdd
Koliesnikova Vieronika <koliesnnikova@gmail.com>
2026-02-25 18:57:45 +0200

    created new wpf project to work on pages and addded new interfaces to services

---
+++ Services/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using StorageClasses;
+
+
+namespace ServicesClasses
+{
+    public interface IDataService
+    {
+        List<DepositaryStorage> GetDepositaryStorages();
+        DepositaryStorage GetDepositaryById(int id);
+        List<ProductsStorage> GetProductsByDepositaryId(int depositaryId);
+        ProductsStorage GetProductsById(int productId);
+        int GetProductCountInDepositary(int depositaryId);
+    }
+}
