191407c7c5ae60b64c9b92783e20f5684ad6763b
stacytrek <stacyhutao@gmail.com>
2026-03-01 23:46:10 +0200

    add ProductDetail page and update project configurations

--- ProductManagerUI/Pages/ProductDetail.xaml.cs
+++ ProductManagerUI/Pages/ProductDetail.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using StorageClasses;
 
 namespace ProductManagerUI.Pages
 {
-    /// <summary>
-    /// Interaction logic for ProductDetail.xaml
-    /// </summary>
     public partial class ProductDetail : Page
     {
-        public ProductDetail()
+        // The designer accepts the selected product
+        public ProductDetail(ProductsStorage product)
         {
             InitializeComponent();
+
+            // Fill the interface with data from Lab1
+            ProductNameText.Text = product.Name;
+            CategoryText.Text = $"Category: {product.Category}";
+            PriceText.Text = $"Price: {product.Price} uah";
+            AmountText.Text = $"Quantity: {product.Amount} pieces.";
+            DescriptionText.Text = product.Description ?? "There is no description";
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack(); // Return to the previous page
+            }
         }
     }
 }
