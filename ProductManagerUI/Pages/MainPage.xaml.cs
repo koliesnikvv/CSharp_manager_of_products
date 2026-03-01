4fafc073aa6e2dc8b74dcc6daaa8c709a82d3b2e
stacytrek <stacyhutao@gmail.com>
2026-03-01 23:28:28 +0200

    add MainWindow frame and MainPage with warehouse list

---
+++ ProductManagerUI/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+using StorageClasses;
+using ServicesClasses;
+
+namespace ProductManagerUI.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Using the correct method name from your ProcessingStorage
+            if (Locator.DataService != null)
+            {
+                WarehouseListBox.ItemsSource = Locator.DataService.GetDepositaryStorages();
+            }
+        }
+
+        private void WarehouseListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (WarehouseListBox.SelectedItem is DepositaryStorage selectedWarehouse)
+            {
+                // Navigate to Level 2: Storage Details and child entities
+                NavigationService.Navigate(new StorageDetailsPage(selectedWarehouse));
+            }
+        }
+    }
+}
